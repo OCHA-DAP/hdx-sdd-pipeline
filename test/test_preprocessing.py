2b956a11b233a99789a32e35b4787fb1ff8b5ea4
liangtelkamp <telkampliang@gmail.com>
2025-10-07 21:38:07 +0200

    chore: Gitignore pycache

--- test/test_preprocessing.py
+++ test/test_preprocessing.py
@@ -1,7 +1,7 @@
-from processing.analyze_xlsx import analyze_excel_file
+from preprocessing.analyze_xlsx import analyze_excel_file
 
 def test_analyze_excel_file():
-    file_path = "processing/tests/test.xlsx"
+    file_path = "preprocessing/tests/test.xlsx"
     output = analyze_excel_file(file_path)
     assert output is not None
     assert output["multiple_tables"] is True
