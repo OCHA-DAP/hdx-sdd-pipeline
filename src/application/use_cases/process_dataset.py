07cdd77fb5548184548988d12449533574359c90
liangtelkamp <telkampliang@gmail.com>
2026-05-29 16:50:52 +0200

    Update process_dataset.py

--- src/application/use_cases/process_dataset.py
+++ src/application/use_cases/process_dataset.py
@@ -343,7 +343,6 @@ class ProcessDatasetUseCase:
             PIIEntityType.EMAIL_ADDRESS,
             PIIEntityType.PHONE_NUMBER,
             PIIEntityType.PERSON_NAME,
-            PIIEntityType.GEO_COORDINATES,
         }
 
         if any(entity_type in sensitive_pii_entities for entity_type in pii_entity_types):
