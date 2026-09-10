387b56401e42603454b04b418dfb75b2b36b372c
liangtelkamp <telkampliang@gmail.com>
2026-09-10 11:05:42 +0200

    presentation

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -5,14 +5,16 @@ from unittest.mock import patch
 from config.config import Config
 
 
-def test_pii_prompt_strategy_defaults_to_local_when_sheet_url_unset():
-    """Test PII prompt strategies default to local when Google Sheet URLs are unset."""
+def test_pii_prompt_strategy_defaults_to_google_sheets_with_fallback_urls():
+    """Test PII prompt strategies fallback to default Google Sheet URLs when env vars are unset."""
     with patch.dict(os.environ, {}, clear=True):
         cfg = Config()
-        assert cfg.PII_DETECTION_GOOGLE_SHEET_URL == ''
-        assert cfg.PII_PROMPT_STRATEGY == 'local'
-        assert cfg.PII_REFLECTION_GOOGLE_SHEET_URL == ''
-        assert cfg.PII_REFLECTION_PROMPT_STRATEGY == 'local'
+        assert cfg.PII_DETECTION_GOOGLE_SHEET_URL == 'https://docs.google.com/spreadsheets/d/1vbn0d3tqZB0dGJTUdBPfn-oRU9m7xPeIwjXH4HW0eYI/edit?gid=0#gid=0'
+        assert cfg.PII_PROMPT_STRATEGY == 'google_sheets'
+        assert cfg.PII_DETECTION_WORKSHEET_NAME == 'PII detection'
+        assert cfg.PII_REFLECTION_GOOGLE_SHEET_URL == 'https://docs.google.com/spreadsheets/d/1vbn0d3tqZB0dGJTUdBPfn-oRU9m7xPeIwjXH4HW0eYI/edit?gid=0#gid=0'
+        assert cfg.PII_REFLECTION_PROMPT_STRATEGY == 'google_sheets'
+        assert cfg.PII_REFLECTION_WORKSHEET_NAME == 'PII reflection'
 
 
 def test_pii_prompt_strategy_defaults_to_google_sheets_when_sheet_url_set():
